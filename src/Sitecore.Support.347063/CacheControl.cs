3cd22b56bafc45c50c1fab0bab576779cdd3aea7
sc-MingShengKhoo <mink@sitecore.net>
2019-07-31 16:57:34 +0800

    Patch Commit; Fix error thrown in the Cache Control Processor if IsRequestBeingRedirected returned true when try to modify cookies.

---
+++ src/Sitecore.Support.347063/CacheControl.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Mvc.Pipelines.MvcEvents.ActionExecuted;
+
+namespace Sitecore.Support.Mvc.Pipelines.MvcEvents.ActionExecuted
+{
+    public class CacheControl : Sitecore.Mvc.Pipelines.MvcEvents.ActionExecuted.CacheControl
+    {
+        protected override void RunCacheControl(ActionExecutedArgs args)
+        {
+            if (Context.HttpContext.Response.IsRequestBeingRedirected)
+            {
+                return;
+            }
+
+            if (base.IsDisableBrowserCaching(args))
+            {
+                base.SetNoCacheNoStore(args);
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.347063/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.347063")]
+[assembly: AssemblyProduct("Sitecore.Support.347063")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("An error occurs in the CacheControl processor if IsRequestBeingRedirected returned true when try to modify cookies")]
